fd7b8f54aa20178feb6102a5c3d14306aee6428a
Jesus Hermano <ehmehe@gmail.com>
2014-08-15 18:07:02 +0200

    Update
    Updated Cho'Gath

--- Cho'Gath/Program.cs
+++ Cho'Gath/Program.cs
@@ -146,7 +146,6 @@ namespace Cho_Gath
 
         private static void ExecuteKillsteal()
         {
-            //test
             foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValidTarget(Q.Range)))
             {
                 if (R.IsReady() && hero.Distance(ObjectManager.Player) <= R.Range &&
