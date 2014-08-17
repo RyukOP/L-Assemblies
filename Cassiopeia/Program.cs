dc91dcdb20027ff306053a265ce01b896ae75f72
Jesus Hermano <ehmehe@gmail.com>
2014-08-18 01:38:41 +0200

    Updated
    Updated Cassio

--- Cassiopeia/Program.cs
+++ Cassiopeia/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
 using LeagueSharp;
 using LeagueSharp.Common;
 using Color = System.Drawing.Color;
@@ -8,12 +10,15 @@ namespace Cassio
 {
     class Program
     {
-        public static Orbwalking.Orbwalker Orbwalker;
-        public static List<Spell> SpellList = new List<Spell>();
-        public static Spell Q;
-        public static Spell W;
-        public static Spell E;
-        public static Menu Config;
+        private static Orbwalking.Orbwalker Orbwalker;
+        private static readonly List<Spell> SpellList = new List<Spell>();
+        private static Spell Q;
+        private static Spell W;
+        private static Spell E;
+        private static Spell R;
+        private static Menu Config;
+
+        private const string ChampionName = "Cassiopeia";
 
         static void Main(string[] args)
         {
@@ -22,47 +27,45 @@ namespace Cassio
 
         private static void Game_OnGameLoad(EventArgs args)
         {
-            try
-            {
-                Q = new Spell(SpellSlot.Q, 850);
-                W = new Spell(SpellSlot.W, 850);
-                E = new Spell(SpellSlot.E, 700);
+            if (ObjectManager.Player.ChampionName != ChampionName)
+                return;
 
-                Q.SetSkillshot(0.60f, 150f, int.MaxValue, false, Prediction.SkillshotType.SkillshotCircle);
-                W.SetSkillshot(0.60f, 300f, int.MaxValue, false, Prediction.SkillshotType.SkillshotCircle);
+            Q = new Spell(SpellSlot.Q, 850);
+            W = new Spell(SpellSlot.W, 850);
+            E = new Spell(SpellSlot.E, 700);
+            R = new Spell(SpellSlot.R, 825);
 
-                SpellList.Add(Q);
-                SpellList.Add(W);
-                SpellList.Add(E);
+            Q.SetSkillshot(0.60f, 150f, int.MaxValue, false, Prediction.SkillshotType.SkillshotCircle);
+            W.SetSkillshot(0.60f, 300f, int.MaxValue, false, Prediction.SkillshotType.SkillshotCircle);
+            R.SetSkillshot(0.30f, 80f , int.MaxValue, false, Prediction.SkillshotType.SkillshotCone);
 
-                Config = new Menu("ESLCassio", "ESLCassio", true);
+            SpellList.Add(Q);
+            SpellList.Add(W);
+            SpellList.Add(E);
 
-                Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
-                Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
+            Config = new Menu(ChampionName, ChampionName, true);
 
-                Config.AddSubMenu(new Menu("Combo", "Combo"));
-                Config.SubMenu("Combo").AddItem(new MenuItem("ComboActive", "Combo!").SetValue(new KeyBind(32, KeyBindType.Press)));
+            Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
+            Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
 
-                Config.AddSubMenu(new Menu("Drawings", "Drawings"));
-                Config.SubMenu("Drawings").AddItem(new MenuItem("QRange", "Q range").SetValue(new Circle(true, Color.FromArgb(255, 255, 255, 255))));
-                Config.SubMenu("Drawings").AddItem(new MenuItem("WRange", "W range").SetValue(new Circle(false, Color.FromArgb(255, 255, 255, 255))));
-                Config.SubMenu("Drawings").AddItem(new MenuItem("ERange", "E range").SetValue(new Circle(false, Color.FromArgb(255, 255, 255, 255))));
-                Config.AddToMainMenu();
+            Config.AddSubMenu(new Menu("Combo", "Combo"));
+            Config.SubMenu("Combo").AddItem(new MenuItem("ComboActive", "Combo!").SetValue(new KeyBind(32, KeyBindType.Press)));
 
-                Drawing.OnDraw += Drawing_OnDraw;
-                Game.OnGameUpdate += Game_OnGameUpdate;
-            }
-            catch (Exception)
-            {
-                Game.PrintChat("Error found in Cassio. Refused to load.");
-            }
+            Config.AddSubMenu(new Menu("Drawings", "Drawings"));
+            Config.SubMenu("Drawings").AddItem(new MenuItem("QRange", "Q range").SetValue(new Circle(true, Color.FromArgb(255, 255, 255, 255))));
+            Config.SubMenu("Drawings").AddItem(new MenuItem("WRange", "W range").SetValue(new Circle(false, Color.FromArgb(255, 255, 255, 255))));
+            Config.SubMenu("Drawings").AddItem(new MenuItem("ERange", "E range").SetValue(new Circle(false, Color.FromArgb(255, 255, 255, 255))));
+            Config.AddToMainMenu();
+
+            Drawing.OnDraw += Drawing_OnDraw;
+            Game.OnGameUpdate += Game_OnGameUpdate;
         }
 
         private static void Drawing_OnDraw(EventArgs args)
         {
             if (SpellList == null) return;
 
-            foreach (Spell spell in SpellList)
+            foreach (var spell in SpellList)
             {
                 var menuItem = Config.Item(spell.Slot + "Range").GetValue<Circle>();
 
@@ -79,34 +82,49 @@ namespace Cassio
 
         static bool IsPoisoned(Obj_AI_Base unit)
         {
-            foreach (var buff in unit.Buffs)
-            {
-                if (buff.IsActive && buff.Type == BuffType.Poison)
-                {
-                    if (buff.EndTime >= (Game.Time + 0.35 + 700 / 1900))
-                        return true;
-
-                }
-            }
-            return false;
+            return unit.Buffs.Where(buff => buff.IsActive && buff.Type == BuffType.Poison).Any(buff => buff.EndTime >= (Game.Time + 0.35 + 700/1900));
         }
 
         private static void ExecuteCombo()
         {
-            Obj_AI_Hero target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Magical);
+            var target = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Magical);
             if (target == null) return;
             
             Orbwalker.SetAttacks(false);
 
-            if (W.IsReady() && ObjectManager.Player.Distance(target) <= W.Range)
-                W.Cast(target, true, true);
+            if (DamageLib.IsKillable(target,
+                new[]
+                {
+                    DamageLib.SpellType.Q, DamageLib.SpellType.W, DamageLib.SpellType.E, DamageLib.SpellType.E,
+                    DamageLib.SpellType.R, DamageLib.SpellType.R, DamageLib.SpellType.IGNITE
+                }))
+            {
+                if (R.IsReady() && ObjectManager.Player.Distance(target) <= R.Range + R.Width)
+                    R.Cast(target, true, true);
 
-            if (Q.IsReady() && ObjectManager.Player.Distance(target) <= Q.Range)
-                Q.Cast(target, true, true);
+                if (W.IsReady() && ObjectManager.Player.Distance(target) <= W.Range + W.Width)
+                    W.Cast(target, true, true);
 
-            if (E.IsReady() && ObjectManager.Player.Distance(target) <= E.Range && IsPoisoned(target) || DamageLib.getDmg(target, DamageLib.SpellType.E) > target.Health)
-                E.CastOnUnit(target, true);
+                if (Q.IsReady() && ObjectManager.Player.Distance(target) <= Q.Range + Q.Width)
+                    Q.Cast(target, true, true);
 
+                if (E.IsReady() && ObjectManager.Player.Distance(target) <= E.Range + target.BoundingRadius && IsPoisoned(target) || DamageLib.IsKillable(target, new[] { DamageLib.SpellType.E }))
+                    E.CastOnUnit(target, true);
+            }
+            else
+            {
+                if (W.IsReady() && ObjectManager.Player.Distance(target) <= W.Range + W.Width)
+                    W.Cast(target, true, true);
+
+                if (Q.IsReady() && ObjectManager.Player.Distance(target) <= Q.Range + Q.Width)
+                    Q.Cast(target, true, true);
+
+                if (E.IsReady() && ObjectManager.Player.Distance(target) <= E.Range + target.BoundingRadius && IsPoisoned(target) || DamageLib.IsKillable(target, new []{DamageLib.SpellType.E}))
+                    E.CastOnUnit(target, true);
+            }
+
+            if (!Q.IsReady() && !W.IsReady() && !E.IsReady())
+                Orbwalker.SetAttacks(true);
         }
     }
 }
