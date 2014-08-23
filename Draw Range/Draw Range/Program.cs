cfdc8e09282d74d38e80c8bf43c74454ff259d83
Jesus Hermano <ehmehe@gmail.com>
2014-08-23 15:32:33 +0200

    Added
    added Range drawer

---
+++ Draw Range/Draw Range/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using Color = System.Drawing.Color;
+
+namespace Draw_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+
+        static void Drawing_OnDraw(EventArgs args)
+        {
+            foreach (var target in ObjectManager.Get<Obj_AI_Hero>().Where(target => target.IsValidTarget(1000)))
+                Drawing.DrawCircle(target.Position, target.AttackRange, Color.Brown);
+        }
+    }
+}
